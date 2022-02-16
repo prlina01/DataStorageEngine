11be0b27a03ebcae7c27ada51127af8fd45b20b4
VladimirGolosin <vladimirgolosin1@gmail.com>
2022-02-16 23:52:54 +0100

    Added map to cache

--- Application/Cache/LRUCache.go
+++ Application/Cache/LRUCache.go
@@ -7,6 +7,7 @@ import (
 
 type Cache struct{
 	LRUlist list.List
+	LRUmap map[string]*list.Element
 	MaxSize int
 }
 
@@ -17,28 +18,31 @@ type Data struct{
 
 func (c *Cache) Init(){
 	c.LRUlist.Init()
+	c.LRUmap = make(map[string]*list.Element)
 }
 
 func (c *Cache) FindKey(key string) *list.Element{
-	for e := c.LRUlist.Front(); e != nil; e = e.Next() {
-		var k Data
-		//Koristiti kasnije kod raspakivanja
-		k = e.Value.(Data)
-		if k.key == key{
-			return e
-		}
+	value, ok := c.LRUmap[key]
+	if ok {
+		return value
+	} else {
+		return nil
 	}
-	return nil
 }
 
 func(c *Cache) PrintAll(){
 	var i int
 	i = 0
+	fmt.Println("lista:")
 	for e := c.LRUlist.Front(); e != nil; e = e.Next() {
 		var k Data
 		k = e.Value.(Data)
 		fmt.Println(i,k.key,k.value)
 		i++
+	}
+	fmt.Println("mapa:")
+	for kljuc, vr := range c.LRUmap {
+		fmt.Println(kljuc, vr)
 	}
 
 }
@@ -46,6 +50,7 @@ func(c *Cache) PrintAll(){
 func (c *Cache) RemoveKey(key string){
 	l := c.FindKey(key)
 	if l!=nil{
+		delete(c.LRUmap,l.Value.(Data).key)
 		c.LRUlist.Remove(l)
 	}
 }
@@ -53,15 +58,20 @@ func (c *Cache) RemoveKey(key string){
 func (c *Cache) AddKV(key string,value []byte){
 	d := Data{key,value}
 	l := c.FindKey(key)
-	if l == nil{
-		if c.LRUlist.Len() == c.MaxSize{
+	if l == nil{ //u slucaju da kljuc ne postoji u cache
+
+		if c.LRUlist.Len() == c.MaxSize{ //ako je cache pun oslobadjamo nastariji element
+			delete(c.LRUmap,c.LRUlist.Back().Value.(Data).key)
 			c.LRUlist.Remove(c.LRUlist.Back())
-			c.LRUlist.PushFront(d)
-			return
 		}
-		c.LRUlist.PushFront(d)
+		c.LRUlist.PushFront(d) //dodavanje kv u listu i mapu
+		c.LRUmap[key]=c.LRUlist.Front()
 		return
 	}
+	// ako kljuc ipak postoji u cache, obrisati postojeci i dodati ga ponovo
+	delete(c.LRUmap,l.Value.(Data).key)
 	c.LRUlist.Remove(l)
+
 	c.LRUlist.PushFront(d)
+	c.LRUmap[key]=c.LRUlist.Front()
 }
